00b5a03373a7e632c04c901d664d56eda601fd8c
Sam V <sam.vidler96@yahoo.com>
2022-12-13 16:17:24 +1000

    Imported Odin Inpsector Plugin

--- Assets/Scripts/InteractionScripts/InteractionHandler.cs
+++ Assets/Scripts/InteractionScripts/InteractionHandler.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InteractionHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+
+    public class LocationChange : MonoBehaviour
     {
-        
+
     }
 }
