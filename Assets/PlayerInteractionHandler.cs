31e766bd5d39222265d68fb3d5194ea46ab7042d
Sam V <sam.vidler96@yahoo.com>
2023-02-23 09:49:10 +1000

    A bunch of stuff I should've pushed sooner

---
+++ Assets/PlayerInteractionHandler.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using cakeslice;
+public class PlayerInteractionHandler : MonoBehaviour
+{
+
+    public GameObject player;
+    private bool interactorFocused = false;
+    private Collider focusCollider;
+    
+
+
+    private void TeleportPlayer(Door door)
+    {
+        player.transform.rotation = door.partnerDoor.transform.rotation;
+        player.transform.position = door.partnerDoor.transform.position + Vector3.back;
+        
+    }
+
+
+    private void Update()
+    {
+        if (interactorFocused)
+        {
+            if (Keyboard.current.eKey.wasPressedThisFrame)
+            {
+                TeleportPlayer(focusCollider.gameObject.GetComponent<Door>());
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+
+        if (other.tag == "Interactable")
+        {
+
+            focusCollider = other;
+            focusCollider.gameObject.GetComponent<Outline>().enabled = true;
+
+
+            interactorFocused = true;
+
+
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Interactable")
+        {
+            interactorFocused = false;
+            focusCollider.gameObject.GetComponent<Outline>().enabled = false;
+        }
+    }
+
+}
