f36cdfac495a2dbd3ed995c7bde0dcc0531bfe9a
Sam V <sam.vidler96@yahoo.com>
2023-05-01 18:58:38 +1000

    Made multiple changes
    - Refactored Interaction code
    - Renamed Mail Room scene to MainTestingScene
    - Created additional Tags for interaction system

--- Assets/Assets/Scripts/PlayerInteractionHandler.cs
+++ Assets/Assets/Scripts/PlayerInteractionHandler.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using cakeslice;
+using Unity.VisualScripting;
+
 public class PlayerInteractionHandler : MonoBehaviour
 {
 
     public GameObject player;
-    private bool interactorFocused = false;
     private Collider focusCollider;
     public UIController UIController;
     public Camera mainCamera;
+
+
+    private bool interactorDoorInBool = false;
+    private bool interactorDoorOutBool = false;
+    private bool interactorItemBool = false;
+    private bool interactorDialogueBool = false;
 
 
     public Animator animator;
@@ -21,65 +28,136 @@ public class PlayerInteractionHandler : MonoBehaviour
 
     private IEnumerator TeleportPlayer(Door door)
     {
-        yield return new WaitForSeconds(0.2f);
-        player.transform.rotation = door.spawnPoint.transform.rotation;
-        player.transform.position = door.spawnPoint.transform.position;
-        mainCamera.clearFlags = CameraClearFlags.SolidColor;
+        if (door.tag == "InteractableDoorIn")
+        {
+            yield return new WaitForSeconds(0.2f);
+            player.transform.rotation = door.spawnPoint.transform.rotation;
+            player.transform.position = door.spawnPoint.transform.position;
+            mainCamera.clearFlags = CameraClearFlags.SolidColor;
+            animator.SetBool("interact", false);
+
+        }
+
+        else 
+        {
+            yield return new WaitForSeconds(0.2f);
+            player.transform.rotation = door.spawnPoint.transform.rotation;
+            player.transform.position = door.spawnPoint.transform.position;
+            mainCamera.clearFlags = CameraClearFlags.Skybox;
+            animator.SetBool("interact", false);
+        }
 
     }
 
 
+    public void CattoInteractor()
+    {
+        if (!interactorDoorInBool && !interactorDoorOutBool && !interactorItemBool && !interactorDialogueBool)
+        {
+            return;
+        }
+
+        if (interactorDoorInBool)
+        {
+            fadeInCoroutine = StartCoroutine(UIController.FadeBlackOutSquare());
+            teleportCoroutine = StartCoroutine(TeleportPlayer(focusCollider.gameObject.GetComponent<Door>()));
+            animator.SetBool("interact", true);
+            
+        }
+
+        if (interactorDoorOutBool)
+        {
+            fadeInCoroutine = StartCoroutine(UIController.FadeBlackOutSquare());
+            teleportCoroutine = StartCoroutine(TeleportPlayer(focusCollider.gameObject.GetComponent<Door>()));
+            animator.SetBool("interact", true);
+
+        }
+
+
+    }
+
     private void Update()
     {
-        if (interactorFocused)
+
+        if (Keyboard.current.eKey.wasPressedThisFrame)
         {
-            if (Keyboard.current.eKey.wasPressedThisFrame)
-            {
-                fadeInCoroutine = StartCoroutine(UIController.FadeBlackOutSquare());
-                teleportCoroutine = StartCoroutine(TeleportPlayer(focusCollider.gameObject.GetComponent<Door>()));
-                //fadeOutCoroutine = StartCoroutine(UIController.FadeBlackOutSquare(false));
-
-                animator.SetBool("interact", true);
-            }
-
-            else
-            {
-                animator.SetBool("interact", false);
-
-            }
+            CattoInteractor();
         }
 
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            mainCamera.clearFlags = CameraClearFlags.Skybox;
-        }
+
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        
 
-        if (other.tag == "InteractableDoor")
+
+
+        switch (other.tag)
         {
-            Debug.Log("interactor focused on a Door");
-            focusCollider = other;
-            focusCollider.gameObject.GetComponent<Outline>().enabled = true;
+            case "InteractableDoorIn":
+                interactorDoorInBool = true;
+                Debug.Log("interactor focused on a Door In");
+                focusCollider = other;
+                focusCollider.gameObject.GetComponent<Outline>().enabled = true;
+                break;
 
-
-            interactorFocused = true;
-
-
+            case "InteractableDoorOut":
+                Debug.Log("interactor focused on a Door Out");
+                focusCollider = other;
+                focusCollider.gameObject.GetComponent<Outline>().enabled = true;
+                interactorDoorOutBool = true;
+                break;
             
+            case "InteractableDialogue":
+                Debug.Log("interactor focused on a person");
+                focusCollider = other;
+                focusCollider.gameObject.GetComponent<Outline>().enabled = true;
+                interactorDialogueBool = true;
+                break;
+            
+            case "InteractableItem":
+                Debug.Log("interactor focused on an item");
+                focusCollider = other;
+                focusCollider.gameObject.GetComponent<Outline>().enabled = true;
+                interactorItemBool = true;
+                break;
+            default:
+                break;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "InteractableDoor")
+
+
+        switch (other.tag)
         {
-            interactorFocused = false;
-            focusCollider.gameObject.GetComponent<Outline>().enabled = false;
+            case "InteractableDoorIn":
+                Debug.Log("interactor stop focused on a Door In");
+                interactorDoorInBool = false;
+                focusCollider.gameObject.GetComponent<Outline>().enabled = false;
+                break;
+            case "InteractableDoorOut":
+                Debug.Log("interactor stop focused on a Door Out");
+                interactorDoorOutBool = false;
+                focusCollider.gameObject.GetComponent<Outline>().enabled = false;
+                break;
+            case "InteractableDialogue":
+                Debug.Log("interactor stop focused on a person");
+                interactorDoorInBool = false;
+                focusCollider.gameObject.GetComponent<Outline>().enabled = false;
+                break;
+            case "InteractableDItem":
+                Debug.Log("interactor stop focused on an item");
+                interactorDoorInBool = false;
+                focusCollider.gameObject.GetComponent<Outline>().enabled = false;
+                break;
+            default:
+                break;
         }
+
+
     }
 
 }
