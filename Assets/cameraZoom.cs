766f685aea75c031c8db25494fbeaad34a9b04d2
Sam V <sam.vidler96@yahoo.com>
2023-05-07 15:01:51 +1000

    Implemented Scroll Wheel Zoom

---
+++ Assets/cameraZoom.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class cameraZoom : MonoBehaviour
+{
+
+    [SerializeField] CinemachineVirtualCamera virtualCamera;
+
+    CinemachineComponentBase componentBase;
+
+    float cameraDistance;
+    [SerializeField] float sensitivity = 10f;
+    [SerializeField] float maxCameraDistance = 20f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (componentBase == null)
+        {
+            componentBase = virtualCamera.GetCinemachineComponent(CinemachineCore.Stage.Body);
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        {
+            cameraDistance = Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+            if (componentBase is Cinemachine3rdPersonFollow)
+            {
+
+                if (cameraDistance < 0)
+                {
+                    (componentBase as Cinemachine3rdPersonFollow).CameraDistance -= cameraDistance;
+                    if ((componentBase as Cinemachine3rdPersonFollow).CameraDistance > maxCameraDistance)
+                    {
+                        (componentBase as Cinemachine3rdPersonFollow).CameraDistance = maxCameraDistance;
+                    }
+
+                }
+
+                else
+                {
+                    (componentBase as Cinemachine3rdPersonFollow).CameraDistance -= cameraDistance;
+                    if ((componentBase as Cinemachine3rdPersonFollow).CameraDistance < 4)
+                    {
+                        (componentBase as Cinemachine3rdPersonFollow).CameraDistance = 4;
+                    }
+                }
+            }
+        }
+    }
+
+
+}
