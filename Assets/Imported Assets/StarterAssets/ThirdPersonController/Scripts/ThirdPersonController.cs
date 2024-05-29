3414e27b599e0300086b57794b20810c6987cf3b
Sam-V <sam.vidler96@yahoo.com>
2024-05-29 20:51:47 +1000

    Exposed private _speed variable on ThirdPersonController

--- Assets/Imported Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/Imported Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -90,7 +90,7 @@ namespace StarterAssets
         public bool _cinemachineRotSuspended = false;
 
         // player
-        private float _speed;
+        public float _speed;
         private float _animationBlend;
         private float _targetRotation = 0.0f;
         private float _rotationVelocity;
