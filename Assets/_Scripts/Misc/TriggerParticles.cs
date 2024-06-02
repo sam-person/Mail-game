03f5966be8905841486bbb8c359689037f2c83e5
Jack <jackdunphy983@gmail.com>
2024-06-03 09:55:35 +1000

    Wooden Crate, leaf pile sound effect

--- Assets/_Scripts/Misc/TriggerParticles.cs
+++ Assets/_Scripts/Misc/TriggerParticles.cs
@@ -6,13 +6,33 @@ public class TriggerParticles : MonoBehaviour
 {
     public ParticleSystem particleSystemToPlay;
     public float speedToTriggerParticles = 3;
+    public bool playSound = false;
+    public AudioClip audioClip; 
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        audioSource.clip = audioClip;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (particleSystemToPlay != null && other.tag == "Player")
         {
-            if(other.GetComponent<StarterAssets.ThirdPersonController>()._speed > speedToTriggerParticles)
-            particleSystemToPlay.Play();
+            if (other.GetComponent<StarterAssets.ThirdPersonController>()._speed > speedToTriggerParticles)
+            {
+                particleSystemToPlay.Play();
+                if (playSound && audioClip != null && !audioSource.isPlaying)
+                {
+                    audioSource.Play();
+                }
+            }
         }
     }
 }
