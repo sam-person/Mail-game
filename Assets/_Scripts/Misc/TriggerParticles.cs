3011fe238e22e0d2c54659e701e968e659ed5a22
Jack <jackdunphy983@gmail.com>
2024-06-14 12:06:09 +1000

    Garden sign, added interactive leaf effect to all bushes

--- Assets/_Scripts/Misc/TriggerParticles.cs
+++ Assets/_Scripts/Misc/TriggerParticles.cs
@@ -5,32 +5,60 @@ using UnityEngine;
 public class TriggerParticles : MonoBehaviour
 {
     public ParticleSystem particleSystemToPlay;
+    public GameObject ParticleSystemPrefab;
     public float speedToTriggerParticles = 3;
     public bool playSound = false;
     public AudioClip audioClip; 
     private AudioSource audioSource;
+    public bool triggerOnExit = false;
+    public bool spawnAtTriggerPoint = false;
 
     private void Start()
     {
-        audioSource = GetComponent<AudioSource>();
-        if (audioSource == null)
+        if(!spawnAtTriggerPoint)
         {
-            audioSource = gameObject.AddComponent<AudioSource>();
-        }
+            audioSource = GetComponent<AudioSource>();
+            if (audioSource == null)
+            {
+                audioSource = gameObject.AddComponent<AudioSource>();
+            }
 
-        audioSource.clip = audioClip;
+            audioSource.clip = audioClip;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
+    {
+        PlayLeafEffect(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(triggerOnExit)
+        {
+            PlayLeafEffect(other);
+        }
+    }
+
+    private void PlayLeafEffect(Collider other)
     {
         if (particleSystemToPlay != null && other.tag == "Player")
         {
             if (other.GetComponent<StarterAssets.ThirdPersonController>()._speed > speedToTriggerParticles)
             {
-                particleSystemToPlay.Play();
-                if (playSound && audioClip != null && !audioSource.isPlaying)
+                if(spawnAtTriggerPoint)
                 {
-                    audioSource.Play();
+                    GameObject spawnedParticles = Instantiate(ParticleSystemPrefab, other.transform.position, transform.rotation);
+                    //other.ClosestPointOnBounds(transform.position)
+                    Debug.Log("Spawning leaf particles");
+                }
+                else
+                {
+                    particleSystemToPlay.Play();
+                    if (playSound && audioClip != null && !audioSource.isPlaying)
+                    {
+                        audioSource.Play();
+                    }
                 }
             }
         }
